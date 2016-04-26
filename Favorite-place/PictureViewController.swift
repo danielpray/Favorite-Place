a4e3d98ca6f3e39ceb64b62d630ae9551b79cadd
Daniel Ray <danielray@daniels-macbook-pro.local>
2016-04-25 17:12:32 -0700

    Initial Version

---
+++ Favorite-place/PictureViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PictureViewController.swift
+//  Favorite-place
+//
+//  Created by Daniel Ray on 4/24/16.
+//  Copyright © 2016 Daniel Ray. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackButtonPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
